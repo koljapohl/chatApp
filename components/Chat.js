72581a6bdd0244a4f455984795d3ad9feeb5f087
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-08-19 09:42:58 +0200

    [CHANGE] default message containing users name

--- components/Chat.js
+++ components/Chat.js
@@ -18,17 +18,17 @@ export default class Chat extends Component {
       messages: [
         {
           _id: 1,
-          text: "Hello developer",
+          text: `Hello ${name}`,
           createdAt: new Date(),
           user: {
             _id: 2,
-            name: "React Native",
+            name: "ChatBot",
             avatar: "https://placeimg.com/140/140/any",
           },
         },
         {
           _id: 2,
-          text: `Hi ${name} you entered the chat`,
+          text: `${name} entered the chat`,
           createdAt: new Date(),
           system: true,
         },
